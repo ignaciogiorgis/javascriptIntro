1873e0394ffa0f89c71322c068a565bf8322a0cb
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2021-11-21 19:34:54 -0300

    ejers2

--- ejers01.js
+++ ejers01.js
@@ -32,3 +32,32 @@ function makeNegative(num) {
   }
 
   //-----------------------------------------------------------------------
+  //Bienvenido. En este kata, se le pide que eleve al cuadrado cada dígito de un número y concatenarlos.
+
+//Por ejemplo, si ejecutamos 9119 a través de la función, saldrá 811181, porque 9 2 es 81 y 1 2 es 1.
+
+function squareDigits(num){
+  return parseInt(Array.from(num.toString(),x => Math.pow(x, 2)).join('')) 
+ }
+
+ //Complete la solución para que divida la cadena en pares de dos caracteres. Si la cadena contiene un número impar de caracteres, entonces debe reemplazar el segundo 
+ //carácter faltante del par final con un guión bajo ('_').
+
+ function solution(str){
+  if(str.length === 0){
+    return [];
+  }else if (str.length%2 === 1){
+    let nString =str.concat('_'); 
+    let array = [];
+     for(let i = 0; i < nString.length ; i= i + 2  ){
+        array.push(nString.slice(i,i+2))
+     }
+     return array; 
+  }
+  let array = [];
+     for(let i = 0; i < str.length ; i= i + 2  ){
+        array.push(str.slice(i,i+2))
+     }
+     return array; 
+    
+}  
